09b3c7c074a703efdb93e1fb78fb7d60494fb368
ClaudiaCerrillo <claudia.benitez@fygsolutions.com>
2016-09-01 16:44:21 -0500

    prueba git hola mundo

--- CuadrillasComun/src/main/java/com/fyg/cuadrillas/comun/HolaMundo.java
+++ CuadrillasComun/src/main/java/com/fyg/cuadrillas/comun/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA KATY");
+		System.out.println("Subelo al Git");
 
 	}
 
